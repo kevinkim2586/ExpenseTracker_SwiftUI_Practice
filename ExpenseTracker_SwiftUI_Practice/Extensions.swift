965f75c181ec6d43f6af944d74aa0842361be6a3
김영채 <kevinkim2586@gmail.com>
2022-03-26 22:34:10 +0900

    feat: DateFormatter extension

--- ExpenseTracker_SwiftUI_Practice/Extensions.swift
+++ ExpenseTracker_SwiftUI_Practice/Extensions.swift
@@ -16,4 +16,16 @@ extension Color {
 
 extension DateFormatter {
     
+    static let allNumericUSA: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy"
+        return formatter
+    }()
+}
+
+extension String {
+    func dateParsed() -> Date {
+        guard let parsedDate = DateFormatter.allNumericUSA.date(from: self) else { return Date() }
+        return parsedDate
+    }
 }
